d32254a132553550e0afcb7593ab34f43ec4aed1
ooozbh <ooozbh@gmail.com>
2018-12-19 10:34:56 +0800

    zhao bohong

--- main.c
+++ main.c
@@ -20,7 +20,7 @@
 #include "touch.h"
 
 u16 col[6] = {RED, YELLOW, GREEN, BLUE, BROWN, LIGHTBLUE};
-
+// 打印方块
 void print_block(u16 x, u16 y, u16 color) {
 	u8 i;
 	POINT_COLOR = color;
@@ -28,7 +28,7 @@ void print_block(u16 x, u16 y, u16 color) {
 		LCD_DrawLine(x, i, x+15, i);
 	}
 }
-
+// 结束界面
 void gameover(void) {
 	u16 i;
 	
@@ -56,7 +56,7 @@ void gameover(void) {
 	LCD_DrawLine(64, 224, 176, 224);
 	LCD_DrawLine(64, 240, 176, 240);
 	LCD_ShowString(96, 150, 200, 16, 16, "Score:");
-	LCD_ShowString(96, 170, 200, 24, 24, "6666");
+	LCD_ShowString(96, 170, 200, 24, 24, "6666"); // 分数位置
 	
 	POINT_COLOR = RED;
 	BACK_COLOR = BLACK;
@@ -76,7 +76,7 @@ void gameover(void) {
 		}
 	}
 }
-
+// 组员界面
 void collaborators(void) {
 	LCD_Clear(BLACK);
 	POINT_COLOR = WHITE;
@@ -113,7 +113,7 @@ void collaborators(void) {
 		}
 	}
 }
-
+// 主界面（菜单））
 void menu(void) {
 	u16 i = 86;
 	u16 j;
@@ -156,7 +156,7 @@ void menu(void) {
 	print_block(224, 16, col[3]);
 	
 }
-
+//主进程
 void main_prog(void) {
 	u8 key;
     u8 i = 0;
@@ -187,87 +187,4 @@ int main(void) {
     tp_dev.init();          //触摸屏初始化
 	LCD_Clear(WHITE);
 	main_prog();
-	/*
-	while (1) {
-		u8 t = KEY_Scan(0);
-		if(KEY0_PRES) {
-			menu();
-		}
-		delay_ms(10);
-		
-		POINT_COLOR = BLUE;
-		if (chg == 1 && flg == 0) {
-			LCD_ShowString(60, 150, 200, 16, 16, "TEMPERATE 00.00C");
-			chg = 0;
-			LCD_Fill(30, 200, 200, 320, WHITE);
-			x_p = 60;
-		} else if (chg == 1 && flg == 1) {
-			LCD_ShowString(60, 150, 200, 16, 16, "TEMPERATE 00.00F");
-			chg = 0;
-			LCD_Fill(30, 200, 200, 320, WHITE);
-			x_p = 60;
-		}
-		key = Remote_Scan();
-		if (key && flg == 0) {
-			flg = 1;
-			chg = 1;
-		} else if (key && flg == 1) {
-			flg = 0;
-			chg = 1;
-		}
-		adcx = T_Get_Adc_Average(ADC_CH_TEMP, 10);
-		tmp = (float)adcx * (3.3 / 4096);
-		k = tmp;
-		adcx = tmp;
-		tmp -= (u8)tmp;
-		LCD_DrawLine(60, 290, 60, 200);
-		LCD_DrawLine(60, 290, 200, 290);
-		LCD_ShowString(200, 270, 200, 16, 16, "T/s");
-		if (flg == 0) {
-			k = (1.43 - k) / 0.0043 + 25;
-			LCD_ShowxNum(140, 150, (u8)k, 2, 16, 0);
-			tmp_k = k;
-			tmp_k -= (u8)tmp_k;
-			LCD_ShowxNum(164, 150, tmp_k * 100, 2, 16, 0X80);
-			LCD_ShowString(60, 200, 200, 16, 16, "TEMPTURE/C");
-			LCD_ShowString(100, 300, 200, 16, 16, "10");
-			LCD_ShowString(140, 300, 200, 16, 16, "20");
-			LCD_ShowString(180, 300, 200, 16, 16, "30");
-			LCD_ShowString(40, 260, 200, 16, 16, "30");
-			LCD_ShowString(40, 220, 200, 16, 16, "40");
-			LCD_DrawPoint(x_p, (u16)(260 - 4 * (k - 30)));
-		} else {
-			k = (1.43 - k) / 0.0043 + 25;
-			k = k * 1.8 + 32;
-			if (k > 100) {
-				k -= 100;
-				tmp_k = k;
-				LCD_ShowxNum(140, 150, (u8)tmp_k, 2, 16, 0);
-				LCD_ShowString(133, 150, 200, 16, 16, "10");
-				tmp_k -= (u8)tmp_k;
-				LCD_ShowxNum(164, 150, tmp_k * 100, 2, 16, 0X80);
-				k += 100;
-			} else {
-				LCD_ShowString(134, 150, 200, 16, 16, " ");
-				tmp_k = k;
-				LCD_ShowxNum(140, 150, (u8)tmp_k, 2, 16, 0);
-				tmp_k -= (u8)tmp_k;
-				LCD_ShowxNum(164, 150, tmp_k * 100, 2, 16, 0X80);
-			}
-			LCD_ShowString(60, 200, 200, 16, 16, "TEMPTURE/F");
-			LCD_ShowString(100, 300, 200, 16, 16, "10");
-			LCD_ShowString(140, 300, 200, 16, 16, "20");
-			LCD_ShowString(180, 300, 200, 16, 16, "30");
-			LCD_ShowString(40, 260, 200, 16, 16, "90");
-			LCD_ShowString(40, 220, 200, 16, 16, "110");
-			LCD_DrawPoint(x_p, (u16)(260 - 2 * (k - 90)));
-		}
-		x_p += 1;
-		if (x_p >= 200)
-			chg = 1;
-		LED0 = !LED0;
-		delay_ms(250);
-		
-	}
-	*/
 }
